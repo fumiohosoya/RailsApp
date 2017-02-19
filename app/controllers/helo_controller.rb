f9970b783b6cafcf8538631537e42d60c3c6d7ea
fumiohosoya <fumioh@cscoms.com>
2017-02-19 13:19:20 +0000

    revisedFeb192017

--- app/controllers/helo_controller.rb
+++ app/controllers/helo_controller.rb
@@ -2,17 +2,10 @@ class HeloController < ApplicationController
     
     
     def index
-        if request.post? then
-            @title = 'Result'
-            if params['check1'] then
-            @msg = 'you Checked!!'
-        else
-            @msg = 'not checked...'
-        end
-    else
-        @title = 'Index'
-        @msg = 'check it...'
-    end
+        @header = 'layout sample'
+        @footer = 'copyright FUMIO-HOSOYA 2017'
+        @title = 'NEW LAYOUT'
+        @msg = 'this is sample page!'
   end
   
 end
