965b6307ee4a985b301b170463fd8d033cd568b5
fumiohosoya <fumioh@cscoms.com>
2017-02-20 13:05:03 +0000

    revisedFEB202018

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  # ここにルーティングの情報を追加する
-  get 'helo/index'
-  get 'helo', to: 'helo#index'
-  get 'helo/other'
-  post 'helo', to: 'helo#index'
-  post 'helo/index'
   
+  get 'dengonban', to: 'dengonban#index'
+  post 'dengonban', to: 'dengonban#index'
+  get 'dengonban/index'
+  post 'dengonban/index'
+
+  get 'helo', to: 'helo#index'
+  post 'helo', to: 'helo#index'
+  get 'helo/index'
+  post 'helo/index'
+
+
+
 end
