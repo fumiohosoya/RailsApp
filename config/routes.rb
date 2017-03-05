4a49d54020c438cc354f6128c35a20d9e73e3531
fumiohosoya <fumioh@cscoms.com>
2017-03-05 14:52:19 +0000

    revisedMar52017

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,19 @@ Rails.application.routes.draw do
   get 'people/index'
 
   get 'people', to: 'people#index'
+  get 'people/add'
+  post 'people/add', to: 'people#create'
   get 'people/:id', to: 'people#show' 
 
   get 'dengonban', to: 'dengonban#index'
   post 'dengonban', to: 'dengonban#index'
   get 'dengonban/index'
   post 'dengonban/index'
-
+  get 'people/edit/:id', to: 'people#edit'
+  post 'people/edit/:id', to: 'people#update'
+  patch 'people/edit/:id', to: 'people#update'
+  get 'people/delete/:id', to: 'people#delete'
+  
   # ここにルーティングの情報を追加する
   #get 'helo/index'
   #get 'helo', to: 'helo#index'
